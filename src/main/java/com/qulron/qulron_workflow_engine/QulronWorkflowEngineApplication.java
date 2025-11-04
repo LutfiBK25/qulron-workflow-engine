126cf63d419121c5a78fe0d0c46171de68056111
Lutfi Alfawadleh <lutfibk25@gmail.com>
2025-11-03 22:09:49 -0500

    Let's Go

---
+++ src/main/java/com/qulron/qulron_workflow_engine/QulronWorkflowEngineApplication.java
@@ -0,0 +1,13 @@
+package com.qulron.qulron_workflow_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QulronWorkflowEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QulronWorkflowEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qulron/qulron_workflow_engine/QulronWorkflowEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qulron.qulron_workflow_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QulronWorkflowEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
